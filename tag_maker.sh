93e3104cc72fcc5aabd1ace5f5a1626303241dfd
kiaragil <kiarargil@gmail.com>
2020-07-17 16:27:01 -0700

    template made

--- tag_maker.sh
+++ tag_maker.sh
@@ -3,18 +3,8 @@ ls *.jpg > img_filenames.txt
 while IFS= read -r line
 do
   	cat >> ready_tags.txt << EOF
-		<a target="_blank" data-lightbox="mygallery" href="images/${line}">
-			<img data-src="images/${line}" alt="" >
-		</a>
+
+  		<a class="swipebox" title="" href="images/${line}"><img src="images/thumbs/${line}"></a>
 
 EOF
 done < img_filenames.txt
-
-
-
-# filename=file.txt
-# for item in 'cat file.txt'; do
-
-
-# 	echo ${item}
-# done
