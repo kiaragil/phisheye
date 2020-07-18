5d17ef13ba09751e5c0ba3fd58c3354bb53b87a5
kiaragil <kiarargil@gmail.com>
2020-07-18 13:16:24 -0700

    maui album up

--- tag_maker.sh
+++ tag_maker.sh
@@ -4,7 +4,7 @@ while IFS= read -r line
 do
   	cat >> ready_tags.txt << EOF
 
-  		<a class="swipebox" title="" href="images/${line}"><img src="images/thumbs/${line}"></a>
+  		<a class="swipebox" title="" href="images/maui/${line}"><img src="images/maui/thumbs/${line}"></a>
 
 EOF
 done < img_filenames.txt
