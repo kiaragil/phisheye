a5169c5e8f43688e4fb9e93087bbea40627b821e
kiaragil <kiarargil@gmail.com>
2020-07-22 20:49:10 -0700

    new photos

--- images/tag_maker.sh
+++ images/tag_maker.sh
@@ -4,8 +4,8 @@ while IFS= read -r line
 do
   	cat >> ready_tags.txt << EOF
 
-  		<a class="swipebox" title="" href="images/hillbomb2/${line}">
-		<img src="images/hillbomb2/thumbs/${line}" alt="" ></a>
+  		<a class="swipebox" title="" href="images/maui/${line}"> <img src="images/maui/thumbs/${line}" alt="" ></a>
+
 
 EOF
 done < img_filenames.txt
